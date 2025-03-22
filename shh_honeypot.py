d17b69b79ed8eac7c3994fe0bb235e35496fd69b
GiovanniAVS <giovanni.soares00@gmail.com>
2025-03-22 12:03:29 -0300

    Create emulated Shell

--- shh_honeypot.py
+++ shh_honeypot.py
@@ -2,6 +2,8 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import socket
+import paramiko
+
 # Constants
 logging_format = logging.Formatter('%(message)s')
 
@@ -35,12 +37,58 @@ def emulated_shell(channel, client_ip):
                 response = b'\n Goodbye!\n'
                 channel.close()
             elif command.strip() == b'pwd':
-                response = b'\n\\usr\local\\' +b'\r\n'
+                response = b'\n' + b'\\usr\local\\' + b'\r\n'
+            elif command.strip() == b'whoami':
+                response = b'\n' + b'corpuser1' + b'\r\n'
+            elif command.strip() == b'ls':
+                response = b'\n' + b'jumpbox1.conf' + b'\r\n'
+            elif command.strip() == b'cat jumpbox1.conf':
+                response = b'\n' + b'Go to Sunday Mass' + b'\r\n'
+            # You can continue to add more commands
+            else:
+                response = b'\n' + bytes(command.strip()) + b'\r\n'
 
         channel.send(response)
         channel.send(b'corporate-jumpbox2$ ')
         command = b""
+
+
 # SSH Server + Socket
 
+class Server(paramiko.ServerInterface):
+
+    def __init__(self, client_ip, input_username=None, input_password=None):
+        self.client_ip = client_ip
+        self.input_username = input_username
+        self.input_password = input_password
+
+    def check_channel_exec_request(self, kind: str, chanid: int) -> int:
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+        
+        #return paramiko.OPEN_FAILED
+        
+    def get_allowed_auths(self):  #With SSH you can use basic auth with user and pass, public and private key criptography 
+        return 'password'
+    
+    def check_auth_password(self, username, password):  #Can be improved using args to capture user input
+        if self.input_username is not None and self.input_password is not None:
+            if username == 'username' and password == 'password':
+                return paramiko.AUTH_SUCCESSFUL
+            else:
+                return paramiko.AUTH_FAILED
+    
+    def check_channel_shell_request(self, channel):
+        self.event.set()
+        return True
+    
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes):
+        return True
+    
+    def check_channel_exec_request(seld, channel, command):
+        command = str(command)
+        return True
+
+            
 
 # Provision SSH-based Honeypot
